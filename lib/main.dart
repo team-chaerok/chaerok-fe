d1557732f2e686d06ed132f3e7c8f87a98405e01
Lee seohuyn <leesophy0805@gmail.com>
2026-07-03 17:11:13 +0900

    feat: AppStartup으로 SDK 초기화 위임, main.dart 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,9 @@
 import 'package:chaerok/app/app.dart';
-import 'package:chaerok/core/config/app_secrets.dart';
+import 'package:chaerok/app/app_startup.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  KakaoSdk.init(nativeAppKey: AppSecrets.kakaoNativeAppKey);
-  await GoogleSignIn.instance.initialize(
-    clientId: AppSecrets.googleClientId,
-    serverClientId: AppSecrets.googleServerClientId,
-  );
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const ChaerokApp(),
-    );
-  }
+  await AppStartup.initialize();
+  runApp(const ChaerokApp());
 }
