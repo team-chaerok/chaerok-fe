93ac1a19c3a4dd770ff343d0c1fd78a2499ad3e6
Lee seohuyn <leesophy0805@gmail.com>
2026-09-16 15:48:37 +0900

    현상결과 페이지 및 릴스 생성 API 연동 : chore : 릴스 재생·저장·공유용 video_player·gal·share_plus 의존성을 추가하고 플랫폼별 플러그인 등록 파일을 갱신 https://github.com/team-chaerok/chaerok-fe/issues/109

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,21 +6,27 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
+import gal
 import geolocator_apple
 import google_sign_in_ios
 import package_info_plus
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import sign_in_with_apple
 import sqlite3_flutter_libs
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
